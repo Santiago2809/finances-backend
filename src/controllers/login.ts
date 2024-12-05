730e277339451848cc3817675cd08bc30410c373
Santiago Cecena <santycc00@hotmail.com>
2024-12-04 18:47:04 -0700

    feat: validate fields and send httpOnly cookie with the token

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -4,7 +4,8 @@ import bcrypt from "bcrypt";
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 import { sign } from 'jsonwebtoken';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { PrismaClientKnownRequestError, PrismaClientValidationError } from '@prisma/client/runtime/library';
+import { isValidEmail, isValidPassword } from '../utils/validate';
 dotenv.config();
 const prisma = new PrismaClient();
 
@@ -17,10 +18,15 @@ const secretKey = process.env.SECRET_KEY!;
 export const loginController = async (req: Request<{}, {}, LoginRequestBody>, res: Response) => {
     const { email, password } = req.body;
 
+    if (!isValidEmail(email) || !isValidPassword(password)) {
+        res.status(400).send({ message: "Please enter valid fields." });
+        return;
+    }
+
     try {
         const { password: userPassword } = await prisma.users.findUniqueOrThrow({ where: { email: email }, select: { password: true } });
 
-        const authenticated = bcrypt.compare(userPassword, password);
+        const authenticated = await bcrypt.compare(password, userPassword);
         if (!authenticated) {
             res.sendStatus(401);
             return;
@@ -35,7 +41,8 @@ export const loginController = async (req: Request<{}, {}, LoginRequestBody>, re
             }
         })
 
-        res.status(200).send({ token })
+        res.cookie("token", token, { httpOnly: true }).sendStatus(200);
+        // res.status(200).send({ token })
         return;
 
     } catch (error: unknown) {
@@ -43,7 +50,8 @@ export const loginController = async (req: Request<{}, {}, LoginRequestBody>, re
             res.status(404).send({ message: "Email not found, please try again." });
             return;
         }
-        res.status(500).send({ message: "something went wrong. Please try again later" });
+
+        res.status(500).send({ message: "Something went wrong. Please try again later" });
         console.log(error);
     }
 }
