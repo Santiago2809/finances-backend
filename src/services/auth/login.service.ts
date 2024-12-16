7384d0b6030d87fecf1c74d2d190f7353a880041
Santiago Cecena <santycc00@hotmail.com>
2024-12-16 02:18:56 -0700

    feat: create login service

---
+++ src/services/auth/login.service.ts
@@ -0,0 +1,47 @@
+import { PrismaClient, users } from "@prisma/client";
+import { AppError } from "../../middlewares/error/errorHandler";
+import { compareHash } from "../../utils/hash";
+
+const prisma = new PrismaClient();
+
+interface LoginData {
+	email: string;
+	password: string;
+}
+
+type User = {
+	id: string;
+	name: string;
+	email: string;
+	phone: string | null;
+};
+
+export const loginUser = async (data: LoginData): Promise<User> => {
+	const { email, password } = data;
+
+	const user = await prisma.users.findUniqueOrThrow({
+		where: { email: email },
+		select: {
+			id: true,
+			name: true,
+			email: true,
+			phone: true,
+			password: true,
+		},
+	});
+	const authenticated = await compareHash(password, user.password);
+
+	if (!authenticated) {
+		throw new AppError("Invalid password.", 401);
+	}
+
+	await prisma.users.update({
+		where: { email: email },
+		data: {
+			last_login: new Date(),
+		},
+	});
+
+	const { password: userpassword, ...rest } = user;
+	return rest;
+};
