70278f22705d6df15f3f413a1690a285d8d8f34d
Santiago Cecena <santycc00@hotmail.com>
2024-12-16 02:15:46 -0700

    feat: create get transactions controller

---
+++ src/controllers/transactions/getTransactions.controller.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError, handlePrismaError } from "../../middlewares/error/errorHandler";
+import { getTransactions } from "../../services/transactions/getTransactions.service";
+
+export const getTransactionsController = async (req: Request, res: Response, next: NextFunction) => {
+	const { userId } = req.params;
+	if (!userId) {
+		next(new AppError("User id not found", 400));
+		return;
+	}
+	try {
+		const transactions = await getTransactions(userId);
+		// console.log(transactions);
+		res.status(200).send({ transactions });
+	} catch (error) {
+		if (error instanceof AppError) {
+			next(error);
+			return;
+		}
+		next(handlePrismaError(error));
+		return;
+	}
+};
