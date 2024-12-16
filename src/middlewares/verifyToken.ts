bbe3521cbc95936019f3ab6d2d1e02c6167e8f42
Santiago Cecena <santycc00@hotmail.com>
2024-12-16 02:17:28 -0700

    feat: create verify token middleware

---
+++ src/middlewares/verifyToken.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError, handleJWTError } from "./error/errorHandler";
+import { decodeToken } from "../services/generateToken";
+
+interface TokenPayload {
+	email: string;
+	id: string;
+}
+
+export const verifyToken = (req: Request, _res: Response, next: NextFunction) => {
+	const token = req.cookies.token;
+	const { userId } = req.params;
+
+	if (!token) {
+		next(new AppError("Token not found", 401));
+		return;
+	}
+	try {
+		const decodedToken = decodeToken(token) as TokenPayload;
+		// console.log(decodedToken);
+		if (decodedToken.id !== userId) {
+			throw new AppError("User ID in the request does not match the token.", 401);
+		}
+		next();
+	} catch (error) {
+		if (error instanceof AppError) {
+			next(error);
+		} else {
+			next(handleJWTError(error));
+		}
+	}
+};
