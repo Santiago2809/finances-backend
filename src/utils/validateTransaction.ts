438103c81d487ed318b15e987e38bdd32ac471ed
Santiago Cecena <santycc00@hotmail.com>
2024-12-18 14:58:29 -0700

    feat: add transaction validation schema using Zod

---
+++ src/utils/validateTransaction.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+export const TransactionSchema = z.object({
+	name: z
+		.string({
+			required_error: "Name is required",
+			invalid_type_error: "Name must be a string",
+		})
+		.min(2)
+		.max(50),
+
+	type: z.string().refine((value) => {
+		if (value !== "income" && value !== "expense") {
+			return false;
+		}
+		return true;
+	}, "Type must be either 'income' or 'expense'"),
+
+	created_at: z.string().datetime({ message: "Date is not valid" }).optional(),
+
+	amount: z.number({ invalid_type_error: "Amount must be a number" }).positive().safe(),
+
+	user_id: z.string().min(10).max(30),
+
+	category_id: z.number({ message: "Category id must be a number" }).positive({ message: "Category id must be a valid id." }).optional(),
+});
