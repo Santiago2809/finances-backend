cab06af4d4940ad0241db9d43f31a7f8bef52f4f
Santiago Cecena <santycc00@hotmail.com>
2024-12-18 14:54:00 -0700

    feat: add routes for retrieving and creating transactions with validation

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,11 +1,21 @@
 import { Router } from "express";
 import { getTransactionsController } from "../controllers/transactions/getTransactions.controller";
 import { verifyToken } from "../middlewares/verifyToken";
+import { getTransactionController } from "../controllers/transactions/getTransaction.controller";
+import { createTransactionController } from "../controllers/transactions/createTransaction.controller";
+import { validateTransaction } from "../middlewares/transactions/validateTransaction";
 
 const router = Router({ mergeParams: true });
 
 router.use(verifyToken);
-//* GET /transactions/ - Get all users transactions.
+
+//* GET /transactions/ - Get all user transactions.
 router.get("/", getTransactionsController);
+
+//* GET /transactions/:id - Get an espeific transaction.
+router.get("/:transactionId", getTransactionController);
+
+//* POST /transactions/ - Create a new transaction
+router.post("/", validateTransaction, createTransactionController);
 
 export default router;
