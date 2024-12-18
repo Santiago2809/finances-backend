4d6b10e735098a42eb08435eb463f34fc1b6b7df
Santiago Cecena <santycc00@hotmail.com>
2024-12-18 15:34:11 -0700

    feat: improve amount precision handling in createTransaction service

--- src/services/transactions/createTransaction.service.ts
+++ src/services/transactions/createTransaction.service.ts
@@ -14,14 +14,14 @@ export const createTransaction = async (data: Transaction) => {
 
 	const dateToInsert = created_at ?? new Date();
 	const categoryToInsert = isNaN(Number(category_id)) ? null : Number(category_id);
+	const truncatedAmount = Math.floor(Number(amount) * 100) / 100;
 
-	//todo: Alter table to fix amount precision.
 	return prisma.transactions.create({
 		data: {
 			name,
 			type,
 			created_at: dateToInsert,
-			amount: parseFloat(Number(amount).toFixed(4)),
+			amount: truncatedAmount,
 			user_id,
 			category_id: categoryToInsert,
 		},
