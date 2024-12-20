599f40ebc9bb6acd21254d185a3af702aed1d470
Santiago Cecena <santycc00@hotmail.com>
2024-12-19 18:20:21 -0700

    feat: add deleteTransaction route for removing a specific transaction

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -4,6 +4,7 @@ import { verifyToken } from "../middlewares/verifyToken";
 import { getTransactionController } from "../controllers/transactions/getTransaction.controller";
 import { createTransactionController } from "../controllers/transactions/createTransaction.controller";
 import { validateTransaction } from "../middlewares/transactions/validateTransaction";
+import { deleteTransactionController } from "../controllers/transactions/deleteTransaction.controller";
 
 const router = Router({ mergeParams: true });
 
@@ -17,5 +18,8 @@ router.get("/:transactionId", getTransactionController);
 
 //* POST /transactions/ - Create a new transaction
 router.post("/", validateTransaction, createTransactionController);
+
+//* DELETE /transactions/:id - Delete a transaction
+router.delete("/:transactionId", deleteTransactionController);
 
 export default router;
