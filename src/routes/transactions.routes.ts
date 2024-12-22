6f05fbfb863d33336b666c5b4dd3a4aaa52b35e5
Santiago Cecena <santycc00@hotmail.com>
2024-12-21 23:24:53 -0700

    feat: add updateTransaction route for updating a specific transaction

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { getTransactionsController } from "../controllers/transactions/getTransactions.controller";
-import { verifyToken } from "../middlewares/verifyToken";
 import { getTransactionController } from "../controllers/transactions/getTransaction.controller";
 import { createTransactionController } from "../controllers/transactions/createTransaction.controller";
-import { validateTransaction } from "../middlewares/transactions/validateTransaction";
 import { deleteTransactionController } from "../controllers/transactions/deleteTransaction.controller";
+import { updateTransactionController } from "../controllers/transactions/updateTransaction.controller";
+import { validateUpdateTransaction } from "../middlewares/transactions/validateUpdateTransaction";
+import { validateTransaction } from "../middlewares/transactions/validateTransaction";
+import { verifyToken } from "../middlewares/verifyToken";
 
 const router = Router({ mergeParams: true });
 
@@ -18,6 +20,9 @@ router.get("/:transactionId", getTransactionController);
 
 //* POST /transactions/ - Create a new transaction
 router.post("/", validateTransaction, createTransactionController);
+
+//* PATCH /transactions/:id - Update a transaction
+router.patch("/:transactionId", validateUpdateTransaction, updateTransactionController);
 
 //* DELETE /transactions/:id - Delete a transaction
 router.delete("/:transactionId", deleteTransactionController);
