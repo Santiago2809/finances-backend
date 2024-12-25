640b9270291f48b2231091bab02fb24fb54a77b0
Santiago Cecena <santycc00@hotmail.com>
2024-12-24 19:15:56 -0700

    feat: add logout route for user authentication

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { loginController } from "../controllers/auth/login.controller";
 import { registerController } from "../controllers/auth/register.controller";
 import { validateBody } from "../middlewares/auth/validateBody";
+import { logoutController } from "../controllers/auth/logout.controller";
 
 const router = Router();
 
 router.post("/login", validateBody("login"), loginController);
 router.post("/register", validateBody("register"), registerController);
-
+router.post("/logout", logoutController);
 export default router;
