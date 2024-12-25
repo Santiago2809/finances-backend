11c56929b501591ca66ab2f380b4a3d1e3f14c3b
Santiago Cecena <santycc00@hotmail.com>
2024-12-25 04:50:50 -0700

    feat: enhance BigInt serialization

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
-import dotenv from 'dotenv';
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-export const {
-    PORT = 3000,
-    SECRET_KEY = "ultra-secret-key-123456789987654321"
-} = process.env;
+(BigInt.prototype as unknown as { toJSON: () => string }).toJSON = function () {
+	return this.toString();
+};
+export const { PORT = 3000, SECRET_KEY = "ultra-secret-key-123456789987654321" } = process.env;
