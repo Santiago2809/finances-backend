b91839f934fa7ed02872679c1f9ec0f115afff30
Santiago Cecena <santycc00@hotmail.com>
2025-08-02 02:48:51 -0700

    refactor: replace generateToken with generateTokens in registerController for token management

--- src/controllers/auth/register.controller.ts
+++ src/controllers/auth/register.controller.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { registerUser } from "../../services/auth/register.service";
-import { generateToken } from "../../services/generateToken";
 import { handlePrismaError } from "../../middlewares/error/errorHandler";
+import { generateTokens } from "../../services/auth/token.service";
 
 export const registerController = async (req: Request, res: Response, next: NextFunction) => {
 	const { name, email, phone, password } = req.body;
@@ -13,8 +13,9 @@ export const registerController = async (req: Request, res: Response, next: Next
 			phone,
 			password,
 		});
-		const token = generateToken({ email, id: createdUser.id });
-		res.status(201).cookie("token", token, { httpOnly: true }).send({ user: createdUser });
+		const { accessToken } = generateTokens({ id: createdUser.id, email: createdUser.email });
+
+		res.status(201).cookie("token", accessToken, { httpOnly: true }).send({ user: createdUser });
 	} catch (error) {
 		next(handlePrismaError(error));
 	}
