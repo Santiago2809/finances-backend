2897ab1aae1f16c9b292472dbcd0f2c902c77320
Santiago Cecena <santycc00@hotmail.com>
2025-08-06 16:34:22 -0700

    feat: refactor token generation logic to improve readability and maintainability

--- src/services/auth/token.service.ts
+++ src/services/auth/token.service.ts
@@ -3,8 +3,18 @@ import { SECRET_KEY } from "../../config";
 import { TokenStorage } from "../../utils/token/TokenStorage";
 
 export const generateTokens = ({ id, email }: { id: string; email: string }) => {
-	const accessToken = sign({ email: email, id: id }, SECRET_KEY, { expiresIn: 3600 });
-	const refreshToken = sign({ email: email, id: id }, SECRET_KEY, { expiresIn: 86400 });
+	const accessToken = sign({ email: email, id: id }, SECRET_KEY, { expiresIn: 60 * 60 * 1 });
+	const refreshToken = sign({ email: email, id: id }, SECRET_KEY, { expiresIn: 60 * 60 * 24 * 14 });
+	TokenStorage.setTokens(accessToken, refreshToken);
+	return { accessToken };
+};
+
+export const deleteTokens = (oldAccessToken: string) => {
+	TokenStorage.deleteTokens(oldAccessToken);
+};
+
+export const refreshAccessToken = ({ id, email, refreshToken }: { id: string; email: string; refreshToken: string }) => {
+	const accessToken = sign({ email: email, id: id }, SECRET_KEY, { expiresIn: 60 * 60 * 1 });
 	TokenStorage.setTokens(accessToken, refreshToken);
 	return { accessToken };
 };
