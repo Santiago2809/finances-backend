75dbd59435aac4ec519e818ad9c5e668c021a0c9
Santiago Cecena <santycc00@hotmail.com>
2025-08-29 02:49:22 -0700

    fix: correct type for expiresIn parameter in generateToken function

--- backend/src/services/generateToken.ts
+++ backend/src/services/generateToken.ts
@@ -1,7 +1,7 @@
 import { sign, verify } from "jsonwebtoken";
 import { SECRET_KEY } from "../config";
 
-export const generateToken = (payload: object, expiresIn: string | number = 3600): string => {
+export const generateToken = (payload: object, expiresIn: number = 3600): string => {
 	return sign(payload, SECRET_KEY, { expiresIn: expiresIn });
 };
 
